e8b12766a59c3a625b1e7c16a6bd4da2079f864a
Joey Jnr <apeelit@gmail.com>
2017-08-18 23:26:20 -0500

    initial commit

---
+++ api/controllers/ArticlesController.js
@@ -0,0 +1,74 @@
+/**
+ * ArticlesController
+ *
+ * @description :: Server-side logic for managing articles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	list:function (req, res){
+		Articles.find({}).exec(function(err, articles){
+			if(err){
+				res.send(500, {error: 'There was a problem with the database'});
+			}
+
+			res.view('list', {articles:articles});
+		});
+	},
+
+	add:function (req, res){
+		res.view('add');
+	},
+
+	create:function(req, res){
+		let title = req.body.title;
+		let body = req.body.body;
+
+		Articles.create({title:title, body:body}).exec(function(err){
+			if(err){
+				res.send(500, {error: 'There was a problem with the database'});
+			}
+
+			res.redirect('/articles/list');
+		});
+	},
+
+	delete:function(req, res){
+		Articles.destroy({id:req.params.id}).exec(function(err){
+			if(err){
+				res.send(500, {error: 'There was a problem with the database'});
+			}
+
+			res.redirect('/articles/list');
+		});
+
+		return false;
+	},
+
+	edit:function(req, res){
+		Articles.findOne({id:req.params.id}).exec(function(err, article){
+			if(err){
+				res.send(500, {error: 'There was a problem with the database'});
+			}
+
+			res.view('edit', {article:article});
+		});
+	},
+
+	update:function(req, res){
+		let title = req.body.title;
+		let body = req.body.body;
+
+		Articles.update({id: req.params.id},{title:title, body:body}).exec(function(err){
+			if(err){
+				res.send(500, {error: 'There was a problem with the database'});
+			}
+
+			res.redirect('/articles/list');
+		});
+
+		return false;
+
+		}
+
+};
